d5f80d39eb0d36e9108a90c0b04bf22a22f0e370
Ubuntu <ubuntu@ip-172-31-28-116.ec2.internal>
2025-10-15 18:12:15 +0000

    Testing GitHub webhook

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ mkdir -p build
 echo "Build successful on $(date)" > build/output.txt
 cat build/output.txt
 echo "This is a new change to test Jenkins build"
+Webhook test Wed Oct 15 18:12:08 UTC 2025
